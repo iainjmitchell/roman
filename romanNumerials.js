7b1bac094e8299f8b89237b293f55e8b36ca923f
iainjmitchell <iainjmitchell@gmail.com>
2013-10-16 20:55:10 +0100

    0 invalid

--- romanNumerials.js
+++ romanNumerials.js
@@ -29,23 +29,50 @@ describe('Someone wants to convert a number into Roman numerial', function(){
 			romanNumerialGenerator.generate(2).should.equal('II');
 		});
 	});
+
+	describe('When they enter 4', function(){
+		it('Then IV is returned', function(){
+			var romanNumerialGenerator = new RomanNumerialGenerator();
+			romanNumerialGenerator.generate(4).should.equal('IV');
+		});
+	});
+
+	describe('When they enter 0', function(){
+		it('Then error is returned', function(){
+			var romanNumerialGenerator = new RomanNumerialGenerator();
+			expect(function(){
+				romanNumerialGenerator.generate(0);
+			}).to.throw(InvalidNumberError);
+		});
+	});
 });
+
+var InvalidNumberError = function(){
+};
 
 var RomanNumerialGenerator = function(){
 	var ROMAN_NUMERIALS = [
 			{ number : 10, symbol : 'X'},
 			{ number : 5, symbol : 'V'},
+			{ number : 4, symbol : 'IV'},
 			{ number : 1, symbol : 'I'}
 		];
 
 	this.generate = function(number){
+		if (number === 0){
+			throw new InvalidNumberError();
+		}
+		return generateNumerial(number);
+	};
+
+	function generateNumerial(number){
 		var romanNumerial = findHighestRomanNumerial(number);
 		if (romanNumerial){
 			var nextNumber = number - romanNumerial.number;
-			return romanNumerial.symbol + this.generate(nextNumber);
+			return romanNumerial.symbol + generateNumerial(nextNumber);
 		}
 		return '';
-	};
+	}
 
 	function findHighestRomanNumerial(number){
 		var count = 0;
