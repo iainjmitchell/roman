e9491413b942b8ec339620f97f6766066fd4bb62
iainjmitchell <iainjmitchell@gmail.com>
2013-10-16 20:21:20 +0100

    V

--- romanNumerials.js
+++ romanNumerials.js
@@ -8,12 +8,22 @@ describe('Someone wants to convert a number into Roman numerial', function(){
 			romanNumerialGenerator.generate(1).should.equal('I');
 		});
 	});
+
+	describe('When they enter 5', function(){
+		it('Then V is returned', function(){
+			var romanNumerialGenerator = new RomanNumerialGenerator();
+			romanNumerialGenerator.generate(5).should.equal('V');
+		});
+	});
 });
 
 var RomanNumerialGenerator = function(){
 	var NUMERIAL_ONE = 'I';
 
-	this.generate = function(){
+	this.generate = function(number){
+		if (number === 5){
+			return 'V';
+		}
 		return NUMERIAL_ONE;
 	};
 };
