f18cf45c168e42e6acbc93354cc49cda3922e84d
iainjmitchell <iainjmitchell@gmail.com>
2013-10-16 20:19:32 +0100

    I

--- romanNumerials.js
+++ romanNumerials.js
@@ -2,6 +2,17 @@ require('chai').should();
 expect = require('chai').expect;
 
 describe('Someone wants to convert a number into Roman numerial', function(){
-	
+	describe('When they enter 1', function(){
+		it('Then I is returned', function(){
+			var romanNumerialGenerator = new RomanNumerialGenerator();
+			romanNumerialGenerator.generate(1).should.equal('I');
+		});
+	});
 });
+
+var RomanNumerialGenerator = function(){
+	this.generate = function(){
+		return 'I';
+	};
+};
 
